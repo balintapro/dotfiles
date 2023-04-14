243e16faf0e4fe5fb3baa043ac7409b34c665cb7
Bálint <abalint88@gmail.com>
2023-04-14 11:53:00 +0200

    zsh and vim refresh 2

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -5,5 +5,4 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {}) 
