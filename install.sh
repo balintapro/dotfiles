c3b13345bc55d9bb71b7b1afede7c449760d12f7
Balint Apro <balint.apro@falcon.io>
2023-12-16 02:11:54 +0100

    feat: install script

--- install.sh
+++ install.sh
@@ -1,37 +1,55 @@
 #!/bin/bash
 
 # Initialize
-DOTFILES_DIR="~/dotfiles"
-CONFIG_DIR="~/.config"
-log_file=~/install_progress_log.txt
+DOTFILES_DIR="$HOME/dotfiles"
+CONFIG_DIR="$HOME/.config"
+CONFIG_FOLDERS=("bash" "mc" "nvim")
 
-# Function to delete folders from ~/.config
-FOLDERS_TO_DELETE="bash|mc|nvim"
+log_file=install_progress_log.txt
 
-for folder in $FOLDERS_TO_DELETE; do
-    folder_path="$CONFIG_DIR/$folder"
-    if [ -d "$folder_path" ]; then
-        rm -rf "$folder_path"
-            echo "Deleted folder: $folder_path" >> $log_file
-            echo "Deleted folder: $folder_path"
-    fi
-done
+# Delete folders from $HOME/dotfiles/.config
+deleteFolders () {
+    for folder in "${CONFIG_FOLDERS[@]}"; do
+        folder_path="$CONFIG_DIR/$folder"
+        if [ -d "$folder_path" ]; then
+            echo "rm - config: $folder_path" >> $log_file
+            echo "rm - config: $folder_path"
+            rm -rf "$folder_path"
+        fi
+    done
+}
 
-# Copy files
-if [ ! -d "$CONFIG_DIR" ]; then
-    mkdir "$CONFIG_DIR"
-fi
+# Copy folders from $HOME/dotfiles/.config to $HOME/.config
+copyFolders () {
+    for folder in "${CONFIG_FOLDERS[@]}"; do
+        folder_path="$DOTFILES_DIR/.config/$folder"
+        echo "cp - config: $folder_path" >> $log_file
+        echo "cp - config: $folder_path"
+        cp -r "$folder_path" "$CONFIG_DIR"
+    done
+}
 
-# Copy files to the home directory
-FILES_TO_EXCLUDE=".config|install.sh|readme.md|.gitignore|.git"
+# Copy dotfiles to the home directory
+overwriteConfigFiles () {
+    for file in "$DOTFILES_DIR"/.[!.]*; do
+        filename=$(basename "$file")
+        if [[ 
+            "$filename" != ".config" && 
+            "$filename" != "install.sh" && 
+            "$filename" != "readme.md" && 
+            "$filename" != ".gitignore" && 
+            "$filename" != ".git" && 
+            "$filename" != ".DS_Store" 
+            ]]; then
+            echo "$HOME/$filename" >> $log_file
+            echo "cp - dotfile $HOME/$filename"
+            cp -r "$file" "$HOME/$filename"
+        fi
+    done
+}
 
-for file in "$DOTFILES_DIR"/*; do
-    filename=$(basename "$file")
-    if [[ "$filename" != "$FILES_TO_EXCLUDE" ]]; then
-        echo "Copied: $file to ~/$filename" >> $log_file
-        echo "Copied: $file to ~/$filename"
-        cp -r "$file" "~/$filename"
-    fi
-done
+deleteFolders
+copyFolders
+overwriteConfigFiles
 
 echo "Dotfiles installation complete!"
